c78e974eb3b158509a17a695ca7cf2f09a8b5576
Abdus Sobhan <abdus.sobhan.vu@gmail.com>
2025-11-03 16:57:39 +0600

    Auto update

--- Luv/C++ Basics/data_types.cpp
+++ Luv/C++ Basics/data_types.cpp
@@ -2,8 +2,16 @@
 using namespace std;
 
 int main(){
-    char c = 'a';
+    char c = 'x';
     int a = 3;
+    double b =3.5;
+    a = 7.5;
+    bool bo = true;
+    long long li = 123456789012345;
 
-    cout << c << " " << a << endl;
+    cout << "Charecter ->: " << c << endl;
+    cout << "Integer ->: " << a << endl;
+    cout << "Double ->: " << b << endl;
+    cout << "Boolean ->: " << bo << endl;
+    cout << "Long Long int ->: " << li << endl;
 }
